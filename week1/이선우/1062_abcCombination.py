d6a0eb73d82c063bfb2834f2740311ff423bf5ab
Sunwoo Lee <swlee6507@gmail.com>
2022-11-03 13:30:35 +0900

    Add files via upload

---
+++ week1/이선우/1062_abcCombination.py
@@ -0,0 +1,20 @@
+iter, abc_count = input1.split()
+alphabetlist = [a ~ z]
+abc_learn = []
+ans = 0
+
+for i in range(0 to iter):
+    wordlist = input() 
+
+for word in wordlist:
+    alphabetlist += add each unique alphabet count
+    for i in range(0 to abc_count):
+        abc_learn = get the letter with highest count
+
+for word in wordlist:
+    word_unique = [get unique alphabet word] ex: 'antarctica' -> 'antrci'
+    word_unique_list = ['a', 'c', 'i', 'n', 'r', 't']
+    if word_unique is in abc_learn
+        ans += 1
+
+print(ans)
