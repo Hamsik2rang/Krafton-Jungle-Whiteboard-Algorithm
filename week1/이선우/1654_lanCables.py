ea682f7e1753b5dc84df4a269b1fc8799ae77a95
Sunwoo Lee <swlee6507@gmail.com>
2022-11-04 14:07:13 +0900

    [이선우/성공] 랜선 자르기(1654)

--- week1/이선우/1654_lanCables.py
+++ week1/이선우/1654_lanCables.py
@@ -1,44 +1,26 @@
 import sys
 
 iter, total_cables = map(int, sys.stdin.readline().split())
-original_list = []
-for i in range(iter):
-    original_list.append(int(sys.stdin.readline()))
-start = 0
-end = max(original_list)
+original_list = [int(sys.stdin.readline()) for _ in range(iter)]
+min_value = 1
+max_value = max(original_list)
 
 def check(cable_length: int, goal: int):
     cable_output = 0
 
     for cable in original_list:
         cable_output += cable // cable_length
-    if cable_output > goal:
-        return 1
-    elif cable_output < goal:
-        return 2
+    if cable_output >= goal:
+        return True
     else:
-        return 3
+        return False
 
+while min_value <= max_value:
+    mid = (min_value + max_value) // 2
 
-
-    
-
-
-
-while True:
-    mid = (start + end)//2
-    case = check(mid, total_cables)
-
-    if case == 1:
-        start = mid
-    elif case == 2:
-        end = mid
+    if check(mid, total_cables):
+        min_value = mid + 1
     else:
-        while case == 3:
-            mid += 1
-            case = check(mid, total_cables)
-        mid -= 1
-        print(mid)
-        break
+        max_value = mid - 1
 
-
+print(max_value)
