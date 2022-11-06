42884c2358d50e4ebfdbbbb857aeb45c92fee117
Im Yongsik <lvhi7121@gmail.com>
2022-11-06 19:44:31 +0900

    [임용식/성공] 별 찍기 - 10(2447)

--- 임용식/2447.py
+++ 임용식/2447.py
@@ -2,7 +2,7 @@ def star(row: int, col: int, n: int) -> None:
     if row // n % 3 == 1 and col // n % 3 == 1:
         print(" ", end="")
     else:
-        if n // 3 == 0:
+        if n == 1:
             print("*", end="")
         else:
             star(row, col, n // 3)
