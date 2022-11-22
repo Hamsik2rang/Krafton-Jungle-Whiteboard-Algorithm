bc09076a9c5f4ec0f7ad953a551029a593e251fd
onebom <bomsister@gmail.com>
2022-11-22 21:10:57 +0900

    [우한봄/ 성공]-연속합2(13398)

--- 우한봄/13398_sum.py
+++ 우한봄/13398_sum.py
@@ -8,15 +8,19 @@ dp=[[0] * n for _ in range(2)]
 # print(int_lst)
 dp[0][0]=int_lst[0]
 
-if n>1:
-    rst=0
-    for idx in range(1,len(int_lst)):
-        dp[0][idx]=max(int_lst[idx]+dp[0][idx-1], int_lst[idx])
-        dp[1][idx]=max(int_lst[idx]+dp[1][idx-1],dp[0][idx-1])
-        
-        # print(dp)
-        rst=max(rst, dp[0][idx], dp[1][idx])
+if max(int_lst)<0:
+    rst=max(int_lst)
+else:
+    if n>1:
 
-else: rst=dp[0][0]
+        rst=0
+        for idx in range(1,len(int_lst)):
+            dp[0][idx]=max(int_lst[idx]+dp[0][idx-1], int_lst[idx])
+            dp[1][idx]=max(int_lst[idx]+dp[1][idx-1],dp[0][idx-1])
+            
+            # print(dp)
+            rst=max(rst, dp[0][idx], dp[1][idx])
+
+    else: rst=dp[0][0]
 
 print(rst)
