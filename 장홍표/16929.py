9da5fea68a9ca70d1f82833fde037a80c4c489df
Hongpyo Jang <remerer@gmail.com>
2022-12-09 17:07:09 +0900

    [장홍표/성공]

--- 장홍표/16929.py
+++ 장홍표/16929.py
@@ -3,33 +3,42 @@ input = sys.stdin.readline
 from collections import deque
 # Two Dots
 
-N, M = map(int, input().strip().split())
+flag = 0
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
-answer = 0
-
+N, M = map(int, input().strip().split())
 array = [list(input().strip()) for _ in range(N)]
-dp = [0 for _ in range(M) for _ in range(N)]
-
-def bfs(x, y, array):
-    q = deque()
-    tmp = array[x][y]
-    array[x][y] = 0
-    q.append((x,y))
-    while(q):
-        q.popleft()
-        for i in range(4):
-            if((0<=(x+dx[i])<M) and (0<=(y+dy[i])<N)):
-                if(array[x+dx[i]][y+dy[i]] == tmp):
-                    q.append((x+dx[i],y+dy[i]))
-                    array[x+dx[i]][y+dy[i]] = 0
-                if(array[x+dx[i]][y+dy[i]] == 0):
-                    answer = 1
-                    break
+q = deque()
+answer = 0
 
 for i in range(N):
     for j in range(M):
-        if(array[i][j] != 0):
-            bfs(i,j,array)
+        if(type(array[i][j])==str):
+            q.append((i,j))
+        while(q):
+            y, x = q.popleft()
+            array[y][x] = ord(array[y][x])
+            count = 0
+            for k in range(4):
+                if(0<=(x+dx[k])<M and 0<=(y+dy[k])<N):
+                    if(type(array[y+dy[k]][x+dx[k]])==str):
+                        if(array[y][x] == ord(array[y+dy[k]][x+dx[k]])):
+                            if(((y+dy[k]), (x+dx[k])) not in q):
+                                q.append(((y+dy[k]), (x+dx[k])))
+                    elif(type(array[y+dy[k]][x+dx[k]])==int):
+                        if(array[y][x] == array[y+dy[k]][x+dx[k]]):
+                            count += 1
+                    if(count == 2):
+                        answer = 1
+                        break
+            if(flag == 1):
+                break
+        if(flag == 1):
+            break
+    if(flag == 1):
+        break
 
-print(answer)
+if(answer == 1):
+    print("Yes")
+else:
+    print("No")
