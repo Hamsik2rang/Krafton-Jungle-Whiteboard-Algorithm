6de2350c1e540eb4be3542c1e6d633ad32dc87ad
hierrr <98255791+hierrr@users.noreply.github.com>
2022-12-16 23:14:49 +0900

    [손창한/성공] 히오스 프로게이머(16564)

--- 손창한/44_16564_hosProgmr.py
+++ 손창한/44_16564_hosProgmr.py
@@ -6,7 +6,6 @@ l = list(int(input()) for _ in range(n))
 
 start = min(l)
 end = start + k
-ret = 0
 while start <= end:
     mid = (start+end)//2
     tmp = 0
@@ -15,7 +14,6 @@ while start <= end:
             tmp += mid - x
     if tmp <= k:
         start = mid+1
-        ret = max(ret, mid)
     else:
         end = mid-1
-print(ret)
+print(end)
