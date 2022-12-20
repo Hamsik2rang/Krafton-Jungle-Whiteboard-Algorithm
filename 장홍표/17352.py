636673701f526ff7d3955ee6ab853d0bb4f3133b
remerer <remerer@gmail.com>
2022-12-21 03:23:45 +0900

    [장홍표/실패] 여러분의 다리가 되어 드리겠습니다!(17352)

--- 장홍표/17352.py
+++ 장홍표/17352.py
@@ -1,3 +1,12 @@
 import sys
 input = sys.stdin.readline
 #여러분의 다리가 되어 드리겠습니다!
+
+N = int(input())
+ck = [0 for i in range(N+1)]
+
+for _ in range(N-2):
+    a, b = map(int, input().strip().split())
+    ck[a] = a
+    ck[b] =ck[a]
+print(ck)
