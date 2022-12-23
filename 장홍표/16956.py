455cbbadacdb93e9ccb7b1e301cb6dad746f47b9
remerer <remerer@gmail.com>
2022-12-23 14:14:04 +0900

    [장홍표/성공] 늑대와 양(16956)

--- 장홍표/16956.py
+++ 장홍표/16956.py
@@ -4,8 +4,36 @@ input = sys.stdin.readline
 
 R, C = map(int, input().strip().split())
 array = [input().strip() for _ in range(R)]
+answer = 1
 
 for i in range(R):
     for j in range(C):
         if array[i][j] == 'S':
-            pass
+            if(0<i):
+                if (array[i-1][j] == 'W'):
+                    answer = 0
+                    break
+            if(i<R-1):
+                if (array[i+1][j] == 'W'):
+                    answer = 0
+                    break
+            if(0<j):
+                if (array[i][j-1] == 'W'):
+                    answer = 0
+                    break
+            if(j<C-1):
+                if (array[i][j+1] == 'W'):
+                    answer = 0
+                    break
+    if(answer == 0):
+        break
+
+                
+# 출력부분
+if(answer):
+    print(answer)
+    for i in range(R):
+        array[i] = array[i].replace('.',"D")
+        print(array[i])
+else:
+    print(0)
