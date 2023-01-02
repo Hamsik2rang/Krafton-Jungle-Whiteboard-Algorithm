8ceef86e5d45024a490bf34ccb7886430ff4991c
Heruing <s_1012@naver.com>
2023-01-02 09:46:29 +0900

    [이현홍/성공]회전초밥(2531)

--- 이현홍/2531 - 회전 초밥.py
+++ 이현홍/2531 - 회전 초밥.py
@@ -4,11 +4,31 @@ sys.stdin = open("input.txt", "r")
 
 import sys
 
-N, d, k, c = map(int, sys.stdin.readline().split())
+# solution1) 33008KB, 64ms
+N, d, k, c = map(int, input().split())
+sushi = list(map(int, sys.stdin.readlines()))
+include = [0] * (d + 1)
+include[c] = 100000
+var = 1
+for idx in range(k):
+    if not include[sushi[idx]]:
+        var += 1
+    include[sushi[idx]] += 1
+mx = var
+for s in range(N):
+    e = (s + k) % N
+    if not include[sushi[e]]:
+        var += 1
+    include[sushi[e]] += 1
+    include[sushi[s]] -= 1
+    if not include[sushi[s]]:
+        var -= 1
+    mx = max(mx, var)
+print(mx)
+
+
+# solution2) 33256KB, 4048ms
+N, d, k, c = map(int, input().split())
 sushi = list(map(int, sys.stdin.readlines()))
 sushi += sushi[:k]
-
-mx = 0
-for i in range(N):
-    mx = max(mx, len(set(sushi[i : i + k]) | set([c])))
-print(mx)
+print(max([len(set(sushi[i : i + k]) | set([c])) for i in range(N)]))
