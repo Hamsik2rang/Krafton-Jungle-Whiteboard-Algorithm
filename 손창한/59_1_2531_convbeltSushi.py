9d0fe711b867f18f235e8df8ef1cdf900bc03a6e
hierrr <98255791+hierrr@users.noreply.github.com>
2023-01-04 07:46:55 +0900

    [손창한/성공] 회전 초밥(2531)

--- 손창한/59_1_2531_convbeltSushi.py
+++ 손창한/59_1_2531_convbeltSushi.py
@@ -2,22 +2,6 @@ import sys
 input = sys.stdin.readline
 
 n, d, k, c = map(int, input().rsplit())
-arr = [int(input().rstrip()) for _ in range(n)]
-lp, rp = 0, 0
-cnt = 0
-
-while lp < n:
-    rp = lp + k # range: lp ~ rp-1
-    dif = set() # how many kinds of
-    flg = True # if coupon used? false : true
-    for i in range(lp, rp):
-        i %= n
-        dif.add(arr[i])
-        if arr[i] == c:
-            flg = False
-    tmp = len(dif)
-    if flg:
-        tmp += 1
-    cnt = max(cnt, tmp)
-    lp += 1
-print(cnt)
+a = [int(input().rstrip()) for _ in range(n)]
+a += a[:k]
+print(max[([len(set(a[i:i+k]) | set([c])) for i in range(n)]))
