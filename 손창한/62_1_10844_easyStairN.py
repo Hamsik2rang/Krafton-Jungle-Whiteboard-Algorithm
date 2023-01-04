6232955be49d389338ce7aeae4aa510c57f88d1f
hierrr <98255791+hierrr@users.noreply.github.com>
2023-01-04 22:36:06 +0900

    [손창한/성공] 쉬운 계단 수(10844)

--- 손창한/62_1_10844_easyStairN.py
+++ 손창한/62_1_10844_easyStairN.py
@@ -1,18 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(i, x):
-    global cnt
-    if not (-1<x<10):
-        return
-    if i == n:
-        cnt += 1
-        return
-    dfs(i+1, x-1)
-    dfs(i+1, x+1)
-
 n = int(input())
-cnt = 0
-for x in range(1, 10):
-    dfs(1, x)
-print(cnt)
+# dp[value][place]
+dp = [[0 for _ in range(10)] for _ in range(101)]
+
+for i in range(1, 10):
+    dp[1][i] = 1
+for i in range(2, n+1):
+    for j in range(10):
+        if j == 0: # only up
+            dp[i][j] = dp[i-1][1]
+        elif j == 9: # only down
+            dp[i][j] = dp[i-1][8]
+        else: # up + down
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+print(sum(dp[n]) % 10**9)
