a6b92c76f0a68b43466a5734f3e4b63d7d8d79cd
hierrr <98255791+hierrr@users.noreply.github.com>
2023-01-04 22:59:18 +0900

    [손창한/성공] 팰린드롬?(10942)

--- 손창한/62_2_10942_palindrome.py
+++ 손창한/62_2_10942_palindrome.py
@@ -1,22 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-def isPal(s, e):
-    m = (s+e)//2
-    pal = []
-    for i in range(s-1, e):
-        if i <= m: # left
-            if i == m and (e-s)%2: # mid
-                continue
-            pal.append(a[i])
-        else: # right
-            if a[i] != pal.pop():
-                return 0
-    return 1
-
 n = int(input())
 a = list(map(int, input().split()))
+
+dp = [[False for _ in range(n)] for _ in range(n)]
+for cnt in range(n):
+    for s in range(n - cnt):
+        e = s + cnt
+        if s == e:
+            # cnt == 0
+            dp[s][e] = True
+        elif a[s] == a[e]:
+            # isPal btwn next(fin) or each sides
+            if s+1 == e or dp[s+1][e-1]:
+                dp[s][e] = True
+
 m = int(input())
 for _ in range(m):
     s, e = map(int, input().split())
-    print(isPal(s-1, e-1))
+    print(1 if dp[s-1][e-1] else 0)
