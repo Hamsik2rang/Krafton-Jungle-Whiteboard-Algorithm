5b6c45bd46dec11b3bfbbf75e766d82c3565f2a5
LemonEaterMJ <116528942+LemonEaterMJ@users.noreply.github.com>
2023-01-04 14:40:19 +0900

    [장지영/성공]외계인의기타연주(2841)

--- 장지영/2841_외계인의기타연주.py
+++ 장지영/2841_외계인의기타연주.py
@@ -4,7 +4,7 @@ from heapq import heappush, heappop
 
 N, P = list(map(int, input().split()))
 
-
+# stack : 116052KB, 356ms
 finger = 0
 
 guitar = {}
@@ -26,5 +26,30 @@ for n in range(N) :
 print(finger)
     
     
-    
+
+# max heapq : 33160KB, 1004ms
+# for n in range(N) : 
+#     line, prat = list(map(int, input().split()))
+#     if guitar[line][0][1] > prat : 
+#         lineLen = len(guitar[line])
+#         for i in range(lineLen) : 
+#             if guitar[line][0][1] > prat : 
+#                 heappop(guitar[line])
+#                 finger += 1     # 하나씩 손가락을 뗀다
+#             elif guitar[line][0][1] == prat : 
+#                 # 이미 있던 손가락이니까 유지 
+#                 break
+#             else :      # originalprat < prat
+#                 heappush(guitar[line], (-prat, prat))
+#                 finger += 1
+#                 break                
+
+#     elif guitar[line][0][1] < prat : # original prat < new prat
+#         heappush(guitar[line], (-prat, prat))   # max heapq
+#         finger += 1     # 원래 프렛에서 손을 안뗀다
+        
+#     else : # 이전 음이랑 같은경우 
+#         continue
+        
+# print(finger)   
     
