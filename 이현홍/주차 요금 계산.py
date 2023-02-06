69c98b473c4ccfb6f0b8a82c6c7ab7e997f552e2
Heruing <s_1012@naver.com>
2023-02-06 23:16:54 +0900

    [이현홍/성공]주차 요금 계산

--- 이현홍/주차 요금 계산.py
+++ 이현홍/주차 요금 계산.py
@@ -4,7 +4,8 @@ def solution(fees, records):
     for idx in range(L):
         records[idx] = records[idx].split()
     records.sort(key=lambda x:x[1])
-    time_dic = {key:0 for key in sorted(set(map(lambda x:x[1], records)))}
+    keys = sorted(set(map(lambda x:x[1], records))
+    time_dic = {key:0 for key in keys)}     # 주의: 각 차량별 하루 총 주차 시간
     
     idx = 0
     while idx < L:
@@ -12,17 +13,19 @@ def solution(fees, records):
         h, m = map(int, records[idx][0].split(":"))
         start = h*60 + m
         end = 24*60 -1
-        if idx == L-1 or records[idx+1][1] != records[idx][1]:
+        
+        if idx == L-1 or records[idx+1][1] != records[idx][1]:  # 짝이 안맞으면 23:59에 나간 것
             idx += 1
         else:
             h, m = map(int, records[idx+1][0].split(":"))
             end = h*60 + m
             idx += 2
         time_dic[number] += end-start
-    for key in sorted(time_dic.keys()):
+    
+    for key in keys:
         pay = fees[1]
         residue = max(0, time_dic[key] - fees[0])
         pay += residue // fees[2] * fees[3]
-        if residue%fees[2]: pay += fees[3]
+        if residue%fees[2]: pay += fees[3]  #나머지 있으면 올림
         answer.append(pay)
     return answer
